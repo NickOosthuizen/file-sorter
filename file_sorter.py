2cbe829a2c8dde8a7a822e27df3d7fd2acd2618d
Nick Oosthuizen <nickoosthuizen@g.ucla.edu>
2020-07-27 17:55:58 -0500

    Separated main function from file_sorter.py to a separate file and changed program to sort when new files are found in the directory instead of a single time

--- file_sorter.py
+++ file_sorter.py
@@ -2,7 +2,7 @@ import os
 
 class Root_Directory():
     def __init__(self, read_file):
-        self.path = ""
+        self.base_path = ""
         self.file_associations = {}
         self.get_file_associations(read_file) 
 
@@ -12,8 +12,8 @@ class Root_Directory():
         if f.mode == "r":
             lines = f.readlines()
             lines = [line.rstrip('\n') for line in lines]
-            self.path = lines[0]
-            if not os.path.isdir(self.path):
+            self.base_path = lines[0]
+            if not os.path.isdir(self.base_path):
                 raise OSError("Specified path to sort is not a directory")
             self.file_associations["other"] = "other_files"
             self.file_associations["dir"] = "directories"
@@ -31,20 +31,20 @@ class Root_Directory():
         f.close()
 
     def update_child_directories(self):
-        current_files = os.listdir(self.path)
+        current_files = os.listdir(self.base_path)
         for dict_name in self.file_associations.values():
             if dict_name not in current_files:
-                dir_path = os.path.join(self.path, dict_name)
+                dir_path = os.path.join(self.base_path, dict_name)
                 current_files.append(dict_name)
                 os.mkdir(dir_path)
     
     def sort_files(self):
-        current_files = os.listdir(self.path)
+        current_files = os.listdir(self.base_path)
         for file_name in current_files:
             if file_name not in self.file_associations.values():
                 (file_ext, new_file_name) = self.resolve_file_name(file_name)
-                old_path = os.path.join(self.path, file_name)
-                new_path = os.path.join(self.path, self.file_associations[file_ext], new_file_name)
+                old_path = os.path.join(self.base_path, file_name)
+                new_path = os.path.join(self.base_path, self.file_associations[file_ext], new_file_name)
                 os.rename(old_path, new_path)
     
     def resolve_file_name(self, file_name):
@@ -52,9 +52,9 @@ class Root_Directory():
         file_ext = "." + split_name[-1]
         if file_ext not in self.file_associations:
             file_ext = "other"
-        if os.path.isdir(os.path.join(self.path, file_name)):
+        if os.path.isdir(os.path.join(self.base_path, file_name)):
             file_ext = "dir"
-        files_in_dir = os.listdir(os.path.join(self.path, self.file_associations[file_ext]))
+        files_in_dir = os.listdir(os.path.join(self.base_path, self.file_associations[file_ext]))
         i = 1
         split_name.insert(1, "")
         while file_name in files_in_dir:
@@ -65,13 +65,7 @@ class Root_Directory():
             i += 1
         return file_ext, file_name
 
-def main():
-    root = Root_Directory("template.txt")
-    root.update_child_directories()
-    root.sort_files()
 
-if __name__ == "__main__":
-    main()
 
 
     
---
+++ main.py
@@ -0,0 +1,16 @@
+import file_sorter
+import time
+import os
+
+def main():
+    root = file_sorter.Root_Directory("template.txt")
+    root.update_child_directories()
+    root.sort_files()
+    number_of_files = len(os.listdir(root.base_path))
+    while True:
+        if len(os.listdir(root.base_path)) != number_of_files:
+            root.sort_files()
+        time.sleep(2)
+
+if __name__ == "__main__":
+    main()
